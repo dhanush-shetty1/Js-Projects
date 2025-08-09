7f533e124b1a6c00b362f51034d3fc0ae2300393
dhanush-shetty1 <dhanush.shetty@somaiya.edu>
2025-08-09 15:47:53 +0530

    Dark mode

---
+++ DarkMode/script.js
@@ -0,0 +1,14 @@
+let black= false;
+
+function mode(){
+    if(!black){
+        document.body.style.backgroundColor='black';
+        black=true;
+    }
+    else{
+        document.body.style.backgroundColor='white';
+        black=false;
+
+    }
+}
+
