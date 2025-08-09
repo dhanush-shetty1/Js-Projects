dd11602b7fc1339e8d69a4e10260611e7c31a9b6
dhanush-shetty1 <dhanush.shetty@somaiya.edu>
2025-08-09 16:07:13 +0530

    Event with buttons

--- Event/script.js
+++ Event/script.js
@@ -1,16 +1,17 @@
 let box=document.getElementById('box');
+let btn=document.getElementById('btn');
 
-box.addEventListener("click",event=>{
-    event.target.style.backgroundColor='red';
-    event.target.textContent='OUCH!!🤕';
+btn.addEventListener("click",event=>{
+    box.style.backgroundColor='red';
+    box.textContent='OUCH!!🤕';
 })
 
-box.addEventListener("mouseover",event=>{
-    event.target.style.backgroundColor='yellow';
-    event.target.textContent='Dont do it🥹';
+btn.addEventListener("mouseover",event=>{
+    box.style.backgroundColor='yellow';
+    box.textContent='Dont do it🥹';
 })
 
-box.addEventListener("mouseout",evenet=>{
-    event.target.style.backgroundColor='lightgreen'
-    event.target.textContent='Dont Click Me 😃';
+btn.addEventListener("mouseout",evenet=>{
+    box.style.backgroundColor='lightgreen'
+    box.textContent='Dont Click Me 😃';
 })
